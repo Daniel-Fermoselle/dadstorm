55b0f989b0be235c6a53c9c7d70020c179b83f07
JoaoBraveCoding <joao.marcal12@hotmail.com>
2016-10-15 22:11:40 +0100

    Started implementing PuppetMaster

---
+++ Dadstorm/ClassLibrary1/ConfigInfo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+
+/*
+ * This class is meant to pass information from the parser to the PuppetMaster it could also be used
+ * to send information from the PuppetMaster to the PCS (needs to implement MarshalByRefObject in that case i think)
+ *///
+namespace Dadstorm
+{
+    public class ConfigInfo
+    {
+        private string operatorId;
+        private ArrayList sourceInput;
+        private int repFactor;
+        private string routing;
+        private ArrayList urls;
+        private string operation;
+        private ArrayList operationParam;
+
+        public ConfigInfo(string operatorId, ArrayList sourceInput, int repFactor, string routing,
+                          ArrayList urls, string operation, ArrayList operationParam)
+        {
+            this.operatorId = operatorId;
+            this.sourceInput = sourceInput;
+            this.repFactor = repFactor;
+            this.routing = routing;
+            this.urls = urls;
+            this.operation = operation;
+            this.operationParam = operationParam;
+        }
+
+        public string OperatorId
+        {
+            set { operatorId = value; }
+            get { return operatorId;  }
+        }
+
+        public ArrayList SourceInput
+        {
+            set { sourceInput = value; }
+            get { return sourceInput;  }
+        }
+
+        public int RepFactor
+        {
+            set { repFactor = value; }
+            get { return repFactor;  }
+        }
+
+        public string Routing
+        {
+            set { routing = value; }
+            get { return routing;  }
+        }
+
+        public ArrayList Urls
+        {
+            set { urls = value; }
+            get { return urls;  }
+        }
+
+        public string Operation
+        {
+            set { operation = value; }
+            get { return operation; }
+        }
+
+        public ArrayList OperationParam
+        {
+            set { operationParam = value; }
+            get { return operationParam;  }
+        }
+
+        /*
+         * TODO add method to add elements to ArrayList atributes
+         * if necessary
+         *///
+    }
+}
---
+++ Dadstorm/PuppetMaster/PuppetMaster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dadstorm
+{
+    class PuppetMaster
+    {
+
+        static void Main(string[] args)
+        {
+            //Start processes phase
+
+            //Receive inputs and log phase
+        }
+
+        public void Start(string operator_id)
+        {
+            //TODO: implement
+        }
+
+        public void Interval(string operator_id, string x_ms)
+        {
+            //TODO: implement
+        }
+
+        public void Status()
+        {
+            //TODO: implement
+        }
+
+        public void Crash(string processname)
+        {
+            //TODO: implement
+        }
+
+        public void Freeze(string processname)
+        {
+            //TODO: implement
+        }
+
+        public void Unfreeze(string processname)
+        {
+            //TODO: implement
+        }
+
+        public void Wait(string x_ms)
+        {
+            //TODO: implement
+        }
+    }
+}
