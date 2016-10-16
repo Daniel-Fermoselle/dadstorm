0bf459e9b650a0ee71dfdd7cafa00bb8a46367d5
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2016-10-16 11:30:31 +0100

    Added ProcessCreationService basica functionality including server registration

---
+++ Dadstorm/ProcessCreationService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using PM_PCS_CommonClass;
+
+namespace ProcessCreationService
+{
+    class PCS
+    {
+        static void Main(string[] args)
+        {
+            //Creating the channel
+            TcpChannel serverChannel = new TcpChannel(10001);
+            ChannelServices.RegisterChannel(serverChannel, false);
+            PCSServer pcss = new PCSServer();
+            RemotingServices.Marshal(pcss, "ProcessCreationServiceServer", typeof(PCSServer));
+            System.Console.WriteLine("<enter> para sair...");
+            System.Console.ReadLine();
+        }
+    }
+}
