b7048f2856dfbbcbb8e0888a376f987b8acc67e9
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2016-10-17 20:23:25 +0100

    Operator added

---
+++ Dadstorm/Operator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Operator
+{
+    class Operator
+    {
+        //private operation o;
+
+        public Operator(/*args*/)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            TcpChannel serverChannel = new TcpChannel(/*porto recebido nos args*/);
+            ChannelServices.RegisterChannel(serverChannel, false);
+            Operator pcss = new Operator();
+        }
+    }
+}
