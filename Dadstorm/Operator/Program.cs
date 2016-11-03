9f04b7ba34986234e0d095c59198727416877918
TiaGoMSR <tiagomsr4@gmail.com>
2016-11-03 20:40:22 +0000

    Operator main almost done Need to populate RepServices class with a method before Marshal

--- Dadstorm/Operator/Program.cs
+++ Dadstorm/Operator/Program.cs
@@ -7,22 +7,29 @@ using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 
-namespace Operator
+namespace Dadstorm
 {
-    class Operator
+
+    public class RepServer
     {
-        //private operation o;
 
-        public Operator(/*args*/)
-        {
-
-        }
+        private const string REPSERVICE_NAME = "RepServices";
 
         static void Main(string[] args)
         {
-            TcpChannel serverChannel = new TcpChannel(/*porto recebido nos args*/);
+            int port = Int32.Parse(args[0]);
+
+            //Creating the channel
+            TcpChannel serverChannel = new TcpChannel(port);
             ChannelServices.RegisterChannel(serverChannel, false);
-            Operator pcss = new Operator();
+
+            // Expose an object form RepServices for remote calls.
+            RepServices repServices = new RepServices();
+            //TODO Call RepServices method to populate class 
+            RemotingServices.Marshal(repServices, REPSERVICE_NAME, typeof(RepServices));
+
+            System.Console.WriteLine("<enter> para sair...");
+            System.Console.ReadLine();
         }
     }
 }
